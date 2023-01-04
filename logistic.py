b9411d5daf0caa0309c8e984cca5199758039994
tjcanger <2052717@tongji.edu.cn>
2023-01-04 20:35:55 +0800

    fix bug

--- logistic.py
+++ logistic.py
@@ -13,7 +13,19 @@ from sklearn.metrics import confusion_matrix
 
 data=pd.read_csv(r'dataset.csv')
 
-data=data.drop(columns=['Unnamed: 0.3','Unnamed: 0.2', 'Unnamed: 0.1', 'Unnamed: 0'])
+data=data.drop(columns=['Unnamed: 0.4','Unnamed: 0.3','Unnamed: 0.2', 'Unnamed: 0.1', 'Unnamed: 0'])
+
+is_crash=data["is_crash"].values
+noacc=0
+hasacc=0
+for i in range(0,len(data)):
+    if is_crash[i]==0:
+        noacc+=1
+    else:
+        hasacc+=1
+print('dataset对照/事故=',float(noacc)/float(hasacc))
+print('全部预测为非事故的准确率=',float(noacc)/float(hasacc+noacc))
+
 
 columns = list(data)
 columns.insert(0, columns.pop(columns.index('is_crash')))
@@ -24,9 +36,9 @@ X = data.iloc[:,1:]
 y = data.iloc[:,0]
 X_train, X_test, y_train, y_test = train_test_split(X,y,random_state=0,test_size=0.3)
 
-# print(X_train.shape)
-# print(X_test.shape)
-classifier = LogisticRegression(random_state=0,max_iter=1000)
+# print(X_test)
+# print(y_test)
+classifier = LogisticRegression(random_state=0,max_iter=1500)
 classifier.fit(X_train, y_train)
 
 y_pred = classifier.predict(X_test)
